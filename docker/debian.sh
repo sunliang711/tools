a78729ccb7291e8204c1a4147973d4041e99be0f
sunliang <sunliang711@163.com>
2018-05-03 15:27:19 +0800

    debian.sh for docker installation

---
+++ docker/debian.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+root(){
+    if [ $EUID -ne 0 ];then
+        echo "Need root privilege"
+        exit 1
+    fi
+}
+
+d8(){
+    root
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    echo 'deb https://apt.dockerproject.org/repo debian-jessie main' >/etc/apt/sources.list.d/docker.list
+    apt-get install apt-transport-https ca-certificates -y
+    apt-get update && sudo apt-get install docker-engine
+
+}
+
+d9(){
+    root
+    apt-get update
+    apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+    apt-key fingerprint 0EBFCD88
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install docker-ce -y
+}
+
+version=$(bash -c "$(curl -fsSL https://raw.githubusercontent.com/sunliang711/tools/master/version.sh)")
+
+if [ "$version" == "debian-8" ];then
+    d8
+elif [ "$version" == "debian-9" ]
+    d9
+else
+    echo "Not support you OS."
+    exit 1
+fi
