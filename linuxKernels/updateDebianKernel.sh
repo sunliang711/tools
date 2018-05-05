f85d9d667c7e2d2a43e3a82f0f667e1949d5d87a
sunliang <sunliang711@163.com>
2018-05-05 18:26:31 +0800

    update kernel for debian

--- linuxKernels/updateDebianKernel.sh
+++ linuxKernels/updateDebianKernel.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-# tested on debian 8,9
-# TODO test on ubuntu 14 16 18
+# tested on debian 8,9 ubuntu 16
+# TODO test on ubuntu 14 18
 
 if [ "$EUID" -ne 0 ];then
     echo "Need root privilege!"
     exit 1
 fi
+usage(){
+    cat<<EOF
+Usage: $(basename $0) Options
+
+Options:
+        -h    print help msg
+        -p    set proxy for curl cmd
+EOF
+exit 1
+}
+
+while getopts ":hp:" opt;do
+    case $opt in
+        h)
+            usage
+            ;;
+        p)
+            proxy=$1
+            echo "use proxy: $proxy"
+            ;;
+    esac
+done
+
+shift $(($OPTIND-1))
 
 kernelURL=${1:-"http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.12/linux-image-4.14.12-041412-generic_4.14.12-041412.201801051649_amd64.deb"}
 
-wget "$kernelURL"
+if [ -n "$proxy" ];then
+    curl -LO -x "$proxy" "$kernelURL" || { echo "Download kernel failed!"; exit 1; }
+else
+    curl -LO "$kernelURL" || { echo "Download kernel failed!"; exit 1; }
+fi
 
 okernel="linux-image-$(uname -r)"
 dpkg -i linux-image-4.*.deb || { echo "install new kernel error!"; exit 1; }
@@ -24,3 +52,4 @@ read -p "reboot ? [y/n]" re
 if [ "$re" == "y" ];then
     reboot
 fi
+
