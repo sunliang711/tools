b72c318bb348898b537f0c076924dd5f65ed097c
sunliang <sunliang711@163.com>
2018-05-05 14:13:10 +0800

    delete sudo

--- docker/debian.sh
+++ docker/debian.sh
@@ -11,7 +11,7 @@ d8(){
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     echo 'deb https://apt.dockerproject.org/repo debian-jessie main' >/etc/apt/sources.list.d/docker.list
     apt-get install apt-transport-https ca-certificates -y
-    apt-get update && sudo apt-get install docker-engine
+    apt-get update && apt-get install docker-engine
 
 }
 
