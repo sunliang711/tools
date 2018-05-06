74b479abc38a734add6b404477ec7c770b1d0d68
sunliang <sunliang711@163.com>
2018-05-06 12:52:58 +0800

    grep -P 有些linux版本不支持

--- version.sh
+++ version.sh
@@ -8,14 +8,17 @@ version(){
             versionFile=/etc/os-release
             if [[ -e "$versionFile" ]];then
                 #check linux distribution version
-                release=$(grep '^ID' "$versionFile"| grep -oP '(?<=ID=).+' | tr -d '"')
-                version=$(grep '^VERSION_ID' "$versionFile" | grep -oP '(?<=ID=).+' | tr -d '"')
+                # release=$(grep '^ID' "$versionFile"| grep -oP '(?<=ID=).+' | tr -d '"')
+                release=$(grep '^ID=' /etc/os-release | awk -F= '{print $2}' | tr -d '"')
+                # version=$(grep '^VERSION_ID' "$versionFile" | grep -oP '(?<=ID=).+' | tr -d '"')
+                version=$(grep '^VERSION_ID' /etc/os-release  | awk -F= '{print $2}' | tr -d '"')
                 echo "$release-$version"
             else
                 #CentOS 6 没有/etc/os-release文件
                 versionFile=/etc/centos-release
                 if [[ -e "$versionFile" ]];then
-                    echo -n 'centos-';grep -oP '(?<=release )[0-9.]+' "$versionFile"
+                    # echo -n 'centos-';grep -oP '(?<=release )[0-9.]+' "$versionFile"
+                    echo -n 'centos-';awk '{print $3}' "$versionFile"
                 else
                     ##TODO add other distribution support here
                     echo "Not support your linux distribution now!!" 1>&2
