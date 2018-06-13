46c131def6b6f3a7b37c67d04b9bb2f6db1febec
sunliang <sunliang711@163.com>
2018-06-13 20:04:56 +0800

    fix bug

--- docker/debian.sh
+++ docker/debian.sh
@@ -19,7 +19,7 @@ d9(){
     root
     apt-get update
     apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
     apt-key fingerprint 0EBFCD88
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
     apt-get update
